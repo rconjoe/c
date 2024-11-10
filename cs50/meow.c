313d109c24b01ac7cc80334e7f7bd9e8641ffa1a
trogdor <trogdor@trogdors-MacBook-Air.local>
2024-11-10 02:33:42 -0800

    tighten up

--- cs50/meow.c
+++ cs50/meow.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-    int counter = 3;
-    while (counter > 0)
+    int i = 3;
+    while (i > 0)
     {
         printf("meow\n");
-        counter = counter - 1;
+        i--;
     }
 }
