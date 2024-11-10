9699a309a081ee0742005e02167886a1f0fb9e94
trogdor <trogdor@trogdors-MacBook-Air.local>
2024-11-10 02:40:19 -0800

    for loop instead of while

--- cs50/meow.c
+++ cs50/meow.c
@@ -2,10 +2,15 @@
 
 int main(void)
 {
-    int i = 0;
-    while (i < 3)
+    /*int i = 0;*/
+    /*while (i < 3)*/
+    /*{*/
+    /*    printf("meow\n");*/
+    /*    i++;*/
+    /*}*/
+
+    for (int i = 0; i < 3; i++)
     {
         printf("meow\n");
-        i++;
     }
 }
