05715deedf94c7235a50d347eb674df9111ab39a
trogdor <trogdor@trogdors-MacBook-Air.local>
2024-11-10 02:20:17 -0800

    finish compare

--- cs50/compare.c
+++ cs50/compare.c
@@ -8,6 +8,14 @@ int main(void)
 
     if (x < y)
     {
-        printf("x is less than y");
+        printf("x is less than y\n");
+    }
+    else if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    else
+    {
+        printf("x is equal to y\n");
     }
 }
