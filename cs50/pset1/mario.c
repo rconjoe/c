b6116c777784a0c493d5b33ce22a94742b4fad9e
trogdor <trogdor@trogdors-MacBook-Air.local>
2024-11-10 12:15:41 -0800

    function abstractions

--- cs50/pset1/mario.c
+++ cs50/pset1/mario.c
@@ -1,23 +1,39 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_size(void);
+void print_grid(int size);
+
+
 int main(void)
 {
     // get size of grid
-    int n;
-    do 
-    {
-        n = get_int("Size: ");
-    } 
-    while (n < 1);
+    int n = get_size();
 
     // print grid of bricks
-    for (int i = 0; i < n; i++)
+    print_grid(n);
+}
+
+
+int get_size(void)
+{
+    int n;
+    do
     {
-        for (int j = 0; j < n; j++)
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+    return n;
+}
+
+void print_grid(int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
         {
             printf("#");
         }
-        printf("#\n");
+        printf("\n");
     }
 }
