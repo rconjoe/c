69ebe24bc8156e70a6b48eb30ae7bbddf80e853a
trogdor <trogdor@trogdors-MacBook-Air.local>
2024-11-10 15:40:15 -0800

    use double to increase floating point precision when dividing

--- cs50/calculator.c
+++ cs50/calculator.c
@@ -6,6 +6,6 @@ int main(void)
     long x = get_long("x: ");
     long y = get_long("y: ");
 
-    float z = (float) x / (float) y;
+    double z = (double) x / (double) y;
     printf("%.20f\n", z);
 }
