9b7793cef8796f18b4310c5f5879f05e5cd48405
trogdor <trogdor@trogdors-MacBook-Air.local>
2024-11-10 22:05:33 -0800

    final solution

--- cs50/lab1/population.c
+++ cs50/lab1/population.c
@@ -4,10 +4,21 @@
 int main(void)
 {
     // TODO: Prompt for start size
-    
+    int s, e;
+    do {
+        s = get_int("Start size: ");
+    } while (s < 9);
     // TODO: Prompt for end size
-
+    do {
+        e = get_int("End size: ");
+    } while (e < s);
     // TODO: Calculate number of years until we reach threshold
+    int y = 0;
+    while (s < e) {
+        s = s + (s / 3) - (s / 4);
+        y++;
+    }
 
     // TODO: Print number of years
+    printf("Years: %i\n", y);
 }
