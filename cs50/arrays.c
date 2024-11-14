db9b4ffb68918c1f7d975954f2fd23f879a07a78
trogdor <trogdor@trogdors-MacBook-Air.local>
2024-11-14 14:15:59 -0800

    initial array and loops

--- cs50/arrays.c
+++ cs50/arrays.c
@@ -3,9 +3,11 @@
 
 int main(void)
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int scores[3];
+    for (int i = 0; i < 3; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }    
 
-    printf("Average: %f\n", (score1 + score2 + score3) / (float) 3);
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / (float) 3);
 }
