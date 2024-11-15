a76322d7fd4a950c4e2842b220e1d61473475c5f
trogdor <trogdor@trogdors-MacBook-Air.local>
2024-11-14 23:54:17 -0800

    wip

--- cs50/scores.c
+++ cs50/scores.c
@@ -3,7 +3,7 @@
 
 const int N = 3;
 
-float average(int array[]);
+float average(int length, int array[]);
 
 int main(void)
 {
@@ -13,15 +13,15 @@ int main(void)
         scores[i] = get_int("Score: ");
     }    
 
-    printf("Average: %f\n", average(scores));
+    printf("Average: %f\n", average(N, scores));
 }
 
-float average(int array[])
+float average(int length, int array[])
 {
     int sum = 0;
     for (int i = 0; i < N; i++)
     {
         sum = sum + array[i];
     }
-    return sum / (float) N;
+    return sum / (float) length;
 }
